e71371809602c7b48569e8f1070fe24015ea9946
Abdel Mfossa <abdelemfossa@gmail.com>
2021-04-17 22:58:21 +0100

    Use an external package : add a random text on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World, Abdel'),
+        body: Center(
+          // child: const Text('Hello World, Abdel'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
