16a77a5b455e7ddaeaa0c93b9ad8f147801b837a
Abdel Mfossa <abdelemfossa@gmail.com>
2021-04-18 00:11:22 +0100

    Change the UI using themes

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        // Add the 3 lines from here...
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
